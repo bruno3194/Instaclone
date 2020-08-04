328bb6c4d8b939b310bac1cc97dedc9e8c2f558a
bruno3194 <brunonaveen1999@gmail.com>
2020-08-04 16:54:39 +0530

    OFFICIAL LOGIN,LOGOUT + Enter to onClick(Button)+ PopDown a KeyBoard

---
+++ app/src/main/java/com/bruno/naveen/instaclone/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.bruno.naveen.instaclone;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import es.dmoral.toasty.Toasty;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText et1,et2;
+    private Button b1,b2;
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btnLILogIn:
+
+                ParseUser.logInInBackground(et1.getText().toString(), et2.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user!=null&&e==null)
+                        {
+                            Toasty.success(LoginActivity.this,"Successfully Logged In",Toasty.LENGTH_LONG).show();
+
+                        }
+                        else
+                        {
+                            Toasty.error(LoginActivity.this,"Error : "+e.getMessage(),Toasty.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+
+            break;
+            case R.id.btnLISignUp:
+                finish();
+                break;
+        }
+
+
+    }
+    public void root(View view){
+        InputMethodManager imm=(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("Log In");
+
+        et1=findViewById(R.id.edLogInEmail);
+        et2=findViewById(R.id.edLogInPswd);
+        et2.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i==KeyEvent.KEYCODE_ENTER && keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                    onClick(b1);
+                }
+                return false;
+            }
+        });
+        b1=findViewById(R.id.btnLILogIn);
+        b2=findViewById(R.id.btnLISignUp);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        //TO MAKE SURE NO CURRENT USER IS LOGGED IN (TO AVOID CONFLICTS)(TEMPROARY)
+        if(ParseUser.getCurrentUser()!=null)
+        {
+            ParseUser.getCurrentUser().logOut();
+        }
+    }
+}
--- app/src/main/java/com/bruno/naveen/instaclone/MainActivity.java
+++ app/src/main/java/com/bruno/naveen/instaclone/MainActivity.java
@@ -1,61 +1,67 @@
 package com.bruno.naveen.instaclone;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.SaveCallback;
-
-import java.util.List;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private Button bt,bt2,bt3;
-    private EditText et1,et2,et3,et4,et5;
-    private TextView tv;
-    public static String str="";
-
+private EditText et1,et2,et3;
 
     @Override
     public void onClick(View view) {
-        try{
-        ParseObject details=new ParseObject("Details");
-        details.put("Name",et1.getText().toString());
-        details.put("Age",Integer.parseInt(et2.getText().toString()));
-        details.put("PHONE",Long.parseLong(et3.getText().toString()));
-        details.put("ADDRESS",et4.getText().toString());
-        details.put("email",et5.getText().toString());
-            details.saveInBackground(new SaveCallback() {
+        switch (view.getId()) {
+            case R.id.btnSUSignUp:
+                if (et1.getText().toString().equals("") || et2.getText().toString().equals("") || et3.getText().toString().equals(""))
+                    Toasty.error(MainActivity.this, "FILL ALL THE FIELDS", Toasty.LENGTH_LONG).show();
+                else {
+                    final ParseUser user = new ParseUser();
+                    user.setUsername(et1.getText().toString());
+                    user.setEmail(et2.getText().toString());
+                    user.setPassword(et3.getText().toString());
+                    final ProgressDialog pd = new ProgressDialog(MainActivity.this);
+                    pd.show();
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toasty.success(MainActivity.this, user.getUsername() + " Is Registered Successfully", Toasty.LENGTH_LONG).show();
 
-                @Override
-                public void done(ParseException e) {
-                    if(e==null){
-                        Toasty.success(MainActivity.this,et1.getText().toString()+"is add successfully",Toasty.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toasty.error(MainActivity.this,"Error"+e.toString(),Toasty.LENGTH_SHORT).show();
-                    }
+                            } else {
+                                Toasty.error(MainActivity.this, "Error : " + e.getMessage(), Toasty.LENGTH_LONG).show();
+                            }
+                            pd.cancel();
+                        }
+                    });
                 }
-            });
+                break;
+            case R.id.btnSULogIn:
+                Intent in=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(in);
+
+                break;
+
         }
-        catch(Exception er)
-        {
-            Toasty.error(MainActivity.this,"Error"+er.toString(),Toasty.LENGTH_SHORT).show();
-        }
+    }
+    private Button b1,b2;
+
+    public void rootTapped(View view){
+        InputMethodManager imm=(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
 
     }
 
@@ -63,106 +69,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Sign Up");
         ParseInstallation.getCurrentInstallation().saveInBackground();
         // Toasty.Config.getInstance().tintIcon(true);
         //Toasty.Config.getInstance().setToastTypeface(@NonNull Typeface typeface);
         Toasty.Config.getInstance().setTextSize(25);
        // Toasty.Config.getInstance().allowQueue(true);
         Toasty.Config.getInstance().apply();
-        bt = findViewById(R.id.button);
-        bt2 = findViewById(R.id.btnAll);
-        bt3 = findViewById(R.id.btn3);
-        et1 = findViewById(R.id.EditText);
-        et2 = findViewById(R.id.EditText2);
-        et3 = findViewById(R.id.EditText3);
-        et4 = findViewById(R.id.EditText4);
-        et5 = findViewById(R.id.EditText5);
-        tv = findViewById(R.id.txt);
-        //GETTING SPECIFIC/SINGLE DATA
-        try {
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ParseQuery<ParseObject> pq = ParseQuery.getQuery("Details");
-                    pq.getInBackground("YGJSASylwG", new GetCallback<ParseObject>() {
-                        @Override
-                        public void done(ParseObject object, ParseException e) {
-                            if (e == null && object != null) {
-
-                                tv.setText("NAME : " + object.get("Name") + "\nAGE : " + object.get("Age"));
-                            } else if (e != null) {
-                                Toasty.error(MainActivity.this, "Error" + e.toString(), Toasty.LENGTH_SHORT).show();
-                            }
-
-
-                        }
-                    });
-                }
-            });
-        } catch (Exception err) {
-            Toasty.error(MainActivity.this, "Error" + err.toString(), Toasty.LENGTH_SHORT).show();
-        }
-
-        //GETTING ALL DATA
-        try {
-            bt2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    ParseQuery<ParseObject> pq = ParseQuery.getQuery("Details");
-                    pq.whereGreaterThanOrEqualTo("Age",17);
-                    pq.setLimit(2);
-                    pq.findInBackground(new FindCallback<ParseObject>() {
-
-
-                        @Override
-                        public void done(List<ParseObject> objects, ParseException e) {
-                           MainActivity.str="";
-                            if (objects.size() != 0 && e == null) {
-                                for (ParseObject ob : objects) {
-                                    MainActivity.str = MainActivity.str + "NAME : " + ob.get("Name") + "   AGE : " + ob.get("Age") + "\n";
-                                }
-                            } else if (e != null) {
-                                Toasty.error(MainActivity.this, "Error" + e.toString(), Toasty.LENGTH_SHORT).show();
-                                return;
-                            }
-                            //TO GET IN SINGLE TOAST
-                            Toasty.info(MainActivity.this, "" + MainActivity.str, Toasty.LENGTH_LONG).show();
-                            //TO GET TOAST USING TIMER :)
-//
-//                            Timer t = new Timer();
-//                            t.scheduleAtFixedRate(new TimerTask() {
-//
-//
-//                                @Override
-//                                public void run() {
-//                                    MainActivity.this.runOnUiThread(new Runnable() {
-//                                        @Override
-//                                        public void run() {
-//                                            Toasty.info(MainActivity.this, "" + MainActivity.str, Toasty.LENGTH_LONG).show();
-//
-//                                        }
-//                                    });
-//                                }
-//                            }, 0, 1000);
-
-                        }
-                    });
-                }
-            });
-
-            bt.setOnClickListener(this);
-        }
-        catch (Exception e)
+        et1=findViewById(R.id.edtSignUpName);
+        et2=findViewById(R.id.edtSignUpEmail);
+        et3=findViewById(R.id.edtSignUpPswd);
+        b1=findViewById(R.id.btnSUSignUp);
+        b2=findViewById(R.id.btnSULogIn);
+        //TO MAKE SURE NO CURRENT USER IS LOGGED IN (TO AVOID CONFLICTS)(TEMPROARY)
+        if(ParseUser.getCurrentUser()!=null)
         {
-            Toasty.error(MainActivity.this, "Error" + e.toString(), Toasty.LENGTH_SHORT).show();
-
+            ParseUser.getCurrentUser().logOut();
         }
-        bt3.setOnClickListener(new View.OnClickListener() {
+
+        b1.setOnClickListener(this);
+
+
+        b2.setOnClickListener(this);
+        et3.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View view) {
-                Intent in=new Intent(MainActivity.this,SignupLoginActivity.class);
-                startActivity(in);
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(i==keyEvent.KEYCODE_ENTER && keyEvent.getAction()==KeyEvent.ACTION_DOWN)
+                    onClick(b1);
+                return false;
             }
         });
+
     }
 }
